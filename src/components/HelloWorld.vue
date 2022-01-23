b277b51ea1fa61ed77195c22db629451ca704733
marsic3 <milos.arsic@lotusflare.com>
2022-01-23 20:41:41 +0100

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="typewriter">
+      <h1>Get ready to race 🏁</h1>
+    </div>
+    <VideoBackground :src="require(`@/assets/comingsoon.mp4`)" style="height: 100vh;" />
+    <div class="container">
+      <img alt="Simmaster logo" :src="require(`@/assets/logo.png`)" style="max-height: 250px;">
+    </div>
   </div>
 </template>
 
 <script>
+ import VideoBackground from 'vue-responsive-video-background-player'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  components: { VideoBackground },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  position: absolute;
+  opacity: 0.4;
+  top: 0;
+  left: 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+span {
+  font-size: 2rem;
+  color: white;
+  font-family: "Bull", sans-serif !important;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.typewriter h1 {
+  width: fit-content;
+  color: white;
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: .15em; /* Adjust as needed */
+  animation: 
+    typing 3.5s steps(40, end),
+    blink-caret 1.5s step-end infinite;
 }
-a {
-  color: #42b983;
+
+/* The typing effect */
+@keyframes typing {
+  from { width: 0 }
+  to { width: 25% }
 }
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: red; }
+}
+
 </style>
